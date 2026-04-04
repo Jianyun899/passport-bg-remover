7fb18b47609f9be840aa70985f458c8e6458d8a6
Jianyun899 <Jianyun899@gmail.com>
2026-04-04 22:17:37 +0800

    fix: add blob: to CSP img-src to allow URL.createObjectURL preview images

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.paypal.com https://www.paypalobjects.com",
               "frame-src 'self' https://www.paypal.com",
-              "img-src 'self' data: https://www.paypal.com https://www.paypalobjects.com",
+              "img-src 'self' data: blob: https://www.paypal.com https://www.paypalobjects.com https://lh3.googleusercontent.com",
               "style-src 'self' 'unsafe-inline' https://www.paypalobjects.com",
               "connect-src 'self' https://www.paypal.com https://api-m.paypal.com",
             ].join('; '),
