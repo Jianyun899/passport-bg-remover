5399e1ee9a4bc4bc2389a7636cda16753e49c740
Jianyun899 <Jianyun899@gmail.com>
2026-04-04 21:30:59 +0800

    fix: add CSP headers to allow PayPal sandbox scripts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.sandbox.paypal.com https://www.paypal.com https://www.paypalobjects.com",
+              "frame-src 'self' https://www.sandbox.paypal.com https://www.paypal.com",
+              "img-src 'self' data: https://www.sandbox.paypal.com https://www.paypal.com https://www.paypalobjects.com",
+              "style-src 'self' 'unsafe-inline' https://www.paypalobjects.com",
+              "connect-src 'self' https://www.sandbox.paypal.com https://api-m.sandbox.paypal.com https://www.paypal.com https://api-m.paypal.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
